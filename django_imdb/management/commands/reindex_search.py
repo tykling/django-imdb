e2db398db1829cec27b001bdcbf66c2a5126fb46
Thomas Steen Rasmussen <thomas@gibfest.dk>
2026-01-25 11:24:39 +0100

    add management command to reindex search

---
+++ django_imdb/management/commands/reindex_search.py
@@ -0,0 +1,19 @@
+"""django-imdb management command to reindex search indexes."""
+
+from __future__ import annotations
+
+from django.core.management.base import BaseCommand
+
+from django_imdb.pocketsearch import reindex_pocketsearch
+
+class Command(BaseCommand):
+    """Management command to reindex title search."""
+
+    help = "Reindex title search"
+
+    def handle(self, *args: str, **options: dict[str, str]) -> None:  # noqa: ARG002
+        """Reindex title search data."""
+        self.stdout.write(self.style.SUCCESS("Beginning reindex"))
+        # Index movies only for now.
+        reindex_pocketsearch(types=["movie"])
+        self.stdout.write(self.style.SUCCESS("Reindex done!"))
